9c691f1ca3698bdecf0d22a4cdf3bf455a357ceb
Brent Gees <brentgees@gmail.com>
2026-06-02 23:54:35 +0200

    Remove service worker event listeners
    Remove service worker event listeners for install, activate, and fetch.

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') return;
-  e.respondWith(fetch(e.request));
-});
+
